82c04ad1cf5d439678634a5b5404ba1b60cba792
CooperHub <phupha.p5@gmail.com>
2025-09-14 23:05:55 +0700

    Update fish it kaitun.lua
    Kaitun Sisyphus

--- fish it kaitun.lua
+++ fish it kaitun.lua
@@ -4,145 +4,59 @@ local Character = Player.Character or Player.CharacterAdded:Wait()
 local HRP = Character:WaitForChild("HumanoidRootPart")
 local Humanoid = Character:WaitForChild("Humanoid")
 
-function allinoneloop()
-        --loop anti afk
-    while true do
-        --Demascus Rod 3k coins
-        local args = {
-        77
-        }
-        game:GetService("ReplicatedStorage"):WaitForChild("Packages"):WaitForChild("_Index"):WaitForChild("sleitnick_net@0.2.0"):WaitForChild("net"):WaitForChild("RF/PurchaseFishingRod"):InvokeServer(unpack(args))
-        wait(0.1)
+--location
+local BAY = CFrame.new(94.43900299072266, 17.03352165222168, 2833.9794921875)
+local ENCHANT = CFrame.new(3239.666015625, -1301.03564453125, 1397.5712890625)
+local TREASUREROOM = CFrame.new(-3539.29931640625, -266.3741455078125, -1635.249755859375)
+local SISYPHUS = CFrame.new(-3674.867431640625, -135.0744171142578, -996.6951293945312) --หันหน้าไป -3676.949951171875, -135.0744171142578, -994.6493530273438
+local VOLCANO = CFrame.new(-602.9340209960938, 59.000057220458984, 108.59332275390625) --หันหน้าไป -603.309326171875, 59.000057220458984, 110.06294250488281
+--Vector3
+local BAY_VEC3 = CFrame.new(94.43900299072266, 17.03352165222168, 2833.9794921875)
+local ENCHANT_VEC3 = Vector3.new(3239.666015625, -1301.03564453125, 1397.5712890625)
+local TREASUREROOM_VEC3 = Vector3.new(-3539.29931640625, -266.3741455078125, -1635.249755859375)
+local SISYPHUS_VEC3 = Vector3.new(-3674.867431640625, -135.0744171142578, -996.6951293945312) --หันหน้าไป -3676.949951171875, -135.0744171142578, -994.6493530273438
+local VOLCANO_VEC3 = Vector3.new(-602.9340209960938, 59.000057220458984, 108.59332275390625) --หันหน้าไป -603.309326171875, 59.000057220458984, 110.06294250488281
 
-        --lucky Rod
-        local args = {
-	    4
-        }
-        game:GetService("ReplicatedStorage"):WaitForChild("Packages"):WaitForChild("_Index"):WaitForChild("sleitnick_net@0.2.0"):WaitForChild("net"):WaitForChild("RF/PurchaseFishingRod"):InvokeServer(unpack(args))
-        wait(0.1)
-        --steampunk Rod 250k coins
-        local args = {
-        6
-        }
-        game:GetService("ReplicatedStorage"):WaitForChild("Packages"):WaitForChild("_Index"):WaitForChild("sleitnick_net@0.2.0"):WaitForChild("net"):WaitForChild("RF/PurchaseFishingRod"):InvokeServer(unpack(args))
-        wait(0.1)
+wait(5)
 
-        --Astral Rod 1m coins
-
-        wait(0.1)
-
-        --Ares Rod 3m coins
-        local args = {
-        126
-        }
-        game:GetService("ReplicatedStorage"):WaitForChild("Packages"):WaitForChild("_Index"):WaitForChild("sleitnick_net@0.2.0"):WaitForChild("net"):WaitForChild("RF/PurchaseFishingRod"):InvokeServer(unpack(args))
-        wait(0.1)
-
-        --Demascus Rod 3k coins
-        local args = {
-        77
-        }
-        game:GetService("ReplicatedStorage"):WaitForChild("Packages"):WaitForChild("_Index"):WaitForChild("sleitnick_net@0.2.0"):WaitForChild("net"):WaitForChild("RF/PurchaseFishingRod"):InvokeServer(unpack(args))
-        wait(0.1)
-
-        --lucky Rod
-        local args = {
-	    4
-        }
-        game:GetService("ReplicatedStorage"):WaitForChild("Packages"):WaitForChild("_Index"):WaitForChild("sleitnick_net@0.2.0"):WaitForChild("net"):WaitForChild("RF/PurchaseFishingRod"):InvokeServer(unpack(args))
-        wait(0.1)
-        --steampunk Rod 250k coins
-        local args = {
-        6
-        }
-        game:GetService("ReplicatedStorage"):WaitForChild("Packages"):WaitForChild("_Index"):WaitForChild("sleitnick_net@0.2.0"):WaitForChild("net"):WaitForChild("RF/PurchaseFishingRod"):InvokeServer(unpack(args))
-        wait(0.1)
-
-        --Astral Rod 1m coins
-
-        wait(0.1)
-
-        --Ares Rod 3m coins
-        local args = {
-        126
-        }
-        game:GetService("ReplicatedStorage"):WaitForChild("Packages"):WaitForChild("_Index"):WaitForChild("sleitnick_net@0.2.0"):WaitForChild("net"):WaitForChild("RF/PurchaseFishingRod"):InvokeServer(unpack(args))
-        wait(1)
-        
-        --nature bait 83.5k
-        local args = {
-	    17
-        }
-        game:GetService("ReplicatedStorage"):WaitForChild("Packages"):WaitForChild("_Index"):WaitForChild("sleitnick_net@0.2.0"):WaitForChild("net"):WaitForChild("RF/PurchaseBait"):InvokeServer(unpack(args))
-        wait(0.1)
-        --Chroma Bait 290k
-        local args = {
-	    6
-        }
-        game:GetService("ReplicatedStorage"):WaitForChild("Packages"):WaitForChild("_Index"):WaitForChild("sleitnick_net@0.2.0"):WaitForChild("net"):WaitForChild("RF/PurchaseBait"):InvokeServer(unpack(args))
-
-        wait(1)
-
-        -- กระโดด 1 ครั้ง
-        if Humanoid then
-            Humanoid.Jump = true
-        end
-    end
+function TP(pos)
+    HRP.CFrame = pos
 end
 
+function unpickuprod()
+    game:GetService("ReplicatedStorage"):WaitForChild("Packages"):WaitForChild("_Index"):WaitForChild("sleitnick_net@0.2.0"):WaitForChild("net"):WaitForChild("RE/EquipToolFromHotbar"):FireServer()
+end
 
-function Starter()
-        -- stop auto fish
-    local args = {
-        false
-    }
-    game:GetService("ReplicatedStorage"):WaitForChild("Packages"):WaitForChild("_Index"):WaitForChild("sleitnick_net@0.2.0"):WaitForChild("net"):WaitForChild("RF/UpdateAutoFishingState"):InvokeServer(unpack(args))
+function pickitem(v)
+    game:GetService("ReplicatedStorage"):WaitForChild("Packages"):WaitForChild("_Index"):WaitForChild("sleitnick_net@0.2.0"):WaitForChild("net"):WaitForChild("RE/EquipToolFromHotbar"):FireServer(v)
+end
 
+function autofish(v)
+    game:GetService("ReplicatedStorage"):WaitForChild("Packages"):WaitForChild("_Index"):WaitForChild("sleitnick_net@0.2.0"):WaitForChild("net"):WaitForChild("RF/UpdateAutoFishingState"):InvokeServer(v)
+end
+
+function IsleFarm()
+    --stop auto fish
+    autofish(false)
     wait(1)
-    -- pick up rod
-    local args = {
-        1
-    }
-    game:GetService("ReplicatedStorage"):WaitForChild("Packages"):WaitForChild("_Index"):WaitForChild("sleitnick_net@0.2.0"):WaitForChild("net"):WaitForChild("RE/EquipToolFromHotbar"):FireServer(unpack(args))
-
+    -- cancel pick up rod
+    unpickuprod()
     wait(1)
-    -- teleport
-    local sisyphus = CFrame.new(-3693.771728515625, -135.5744171142578, -1040.6446533203125)
-    local treasure = CFrame.new(-3539.29931640625, -266.3741455078125, -1635.249755859375)
 
-    function TP(Pos)
-        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = Pos
-    end
-
-    TP(sisyphus)
-
-
-    wait(5)
-    -- tween and fish
-    local targetPos = Vector3.new(-3674.867431640625, -135.0744171142578, -996.6951293945312)  -- จุดที่ไป
-    local lookAtPos = Vector3.new(-3676.949951171875, -135.0744171142578, -994.6493530273438)      -- จุดที่หันหน้า
-    local tweenInfo = TweenInfo.new(2, Enum.EasingStyle.Linear, Enum.EasingDirection.Out)
-
-    -- ใช้ CFrame.new(pos, lookAt) ให้ไปและหันหน้า
+    local targetPos = SISYPHUS_VEC3  -- จุดที่ไป
+    local lookAtPos = Vector3.new(-3676.949951171875, -135.0744171142578, -994.6493530273438) -- จุดที่หันหน้า
+    local tweenInfo = TweenInfo.new(5, Enum.EasingStyle.Linear, Enum.EasingDirection.Out)
     local goal = {CFrame = CFrame.new(targetPos, lookAtPos)}
-
     local tween = TweenService:Create(HRP, tweenInfo, goal)
     tween:Play()
 
-    wait(4)
+    wait(10)
+    -- pick up rod
+    pickitem(1)
+    wait(1)
+    --start autofish
+    autofish(true)
 
-    local args = {
-        true
-    }
-    game:GetService("ReplicatedStorage"):WaitForChild("Packages"):WaitForChild("_Index"):WaitForChild("sleitnick_net@0.2.0"):WaitForChild("net"):WaitForChild("RF/UpdateAutoFishingState"):InvokeServer(unpack(args))
-
-    while true do
-        game:GetService("ReplicatedStorage"):WaitForChild("Packages"):WaitForChild("_Index"):WaitForChild("sleitnick_net@0.2.0"):WaitForChild("net"):WaitForChild("RE/FishingCompleted"):FireServer()
-        wait(1)
-    end
 end
 
-Starter()
-allinoneloop()
-
-
+IsleFarm()
